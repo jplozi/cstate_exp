47539a0fee1a784f5e4cbee9684bd68ba178c33a
Jean-Pierre Lozi <jean-pierre.lozi@oracle.com>
2021-09-19 21:13:14 +0200

    Add workaround for unexported kallsyms_lookup_name in newer kernels

--- cstate_exp.c
+++ cstate_exp.c
@@ -5,6 +5,7 @@
 #include <linux/kallsyms.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kprobes.h>
 #include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
@@ -22,6 +23,10 @@ MODULE_DESCRIPTION("A kernel module to test setting a C-state with "
                    "MONITOR/MWAIT.");
 MODULE_VERSION("0.01");
 
+#define KPROBE_PRE_HANDLER(fname) \
+    static int __kprobes fname(struct kprobe *p, struct pt_regs *regs)
+
+typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
 typedef void (*tick_nohz_idle_stop_tick_t) (void);
 typedef void (*tick_nohz_idle_restart_tick_t) (void);
 typedef unsigned int (*aperfmperf_get_khz_t) (int);
@@ -30,13 +35,45 @@ typedef struct task_struct *(*kthread_create_on_cpu_t) (int (*)(void *),
                                                         unsigned int,
                                                         const char *);
 
+static kallsyms_lookup_name_t __kallsyms_lookup_name;
 static tick_nohz_idle_stop_tick_t __tick_nohz_idle_stop_tick;
 static tick_nohz_idle_restart_tick_t __tick_nohz_idle_restart_tick;
 static aperfmperf_get_khz_t __aperfmperf_get_khz;
 static kthread_create_on_cpu_t __kthread_create_on_cpu;
 
+static struct kprobe kp0, kp1;
 struct task_struct *task1, *task2;
 unsigned int shared_var __attribute__((aligned(64))) = 0;
+
+KPROBE_PRE_HANDLER(handler_pre0)
+{
+    __kallsyms_lookup_name = (kallsyms_lookup_name_t) (--regs->ip); 
+    return 0;
+}
+
+KPROBE_PRE_HANDLER(handler_pre1)
+{
+    return 0;
+}
+
+static int do_register_kprobe(struct kprobe *kp, char *symbol_name, void *handler)
+{
+    int ret;
+
+    kp->symbol_name = symbol_name;
+    kp->pre_handler = handler;
+
+    ret = register_kprobe(kp);
+    if (ret < 0) {
+        pr_err("register_probe() for symbol %s failed, returned %d\n",
+               symbol_name, ret);
+        return ret;
+    }
+
+    pr_info("Planted kprobe for symbol %s at %p\n", symbol_name, kp->addr);
+
+    return ret;
+}
 
 static void print_freq(char *str)
 {
@@ -121,24 +158,11 @@ static int thread_mwait(void *data)
 
 static int __init cstate_exp_init(void)
 {
-    struct cpuinfo_x86 *c = &boot_cpu_data;
-
-    __tick_nohz_idle_stop_tick =
-        (tick_nohz_idle_stop_tick_t)
-            kallsyms_lookup_name("tick_nohz_idle_stop_tick");
-    __tick_nohz_idle_restart_tick =
-        (tick_nohz_idle_restart_tick_t)
-            kallsyms_lookup_name("tick_nohz_idle_restart_tick");
-    __aperfmperf_get_khz =
-        (aperfmperf_get_khz_t)
-            kallsyms_lookup_name("aperfmperf_get_khz");
-    __kthread_create_on_cpu =
-        (kthread_create_on_cpu_t)
-            kallsyms_lookup_name("kthread_create_on_cpu");
+    int ret;
 
     write_console("[cstate_exp] Init\r\n");
 
-    if (cpu_has(c, X86_FEATURE_MWAIT))
+    if (cpu_has(&boot_cpu_data, X86_FEATURE_MWAIT))
     {
         write_console("[cstate_exp] MONITOR/MWAIT is supported!\r\n");
     } 
@@ -147,6 +171,32 @@ static int __init cstate_exp_init(void)
         write_console("[cstate_exp] MONITOR/MWAIT is *NOT* supported! "
                       "Continuing anyway...\r\n");
     }
+
+    ret = do_register_kprobe(&kp0, "kallsyms_lookup_name", handler_pre0);
+    if (ret < 0)
+      return ret;
+ 
+    ret = do_register_kprobe(&kp1, "kallsyms_lookup_name", handler_pre1);
+    if (ret < 0) {
+      unregister_kprobe(&kp0);
+      return ret;
+    }
+  
+    unregister_kprobe(&kp0);
+    unregister_kprobe(&kp1);
+  
+    __tick_nohz_idle_stop_tick =
+        (tick_nohz_idle_stop_tick_t)
+            __kallsyms_lookup_name("tick_nohz_idle_stop_tick");
+    __tick_nohz_idle_restart_tick =
+        (tick_nohz_idle_restart_tick_t)
+            __kallsyms_lookup_name("tick_nohz_idle_restart_tick");
+    __aperfmperf_get_khz =
+        (aperfmperf_get_khz_t)
+            __kallsyms_lookup_name("aperfmperf_get_khz");
+    __kthread_create_on_cpu =
+        (kthread_create_on_cpu_t)
+            __kallsyms_lookup_name("kthread_create_on_cpu");
 
     task1 = __kthread_create_on_cpu(&thread_waker, NULL, MWAIT_WAKER,
                                     "thread_waker");
